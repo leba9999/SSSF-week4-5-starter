10156a7658ef4fd14fbc3699ec5fbbb3b1b95cbd
Leevi Koskinen <leevi.koskinen2@metropolia.fi>
2023-04-16 13:50:36 +0300

    Ready to deploy

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
       /* eslint-disable no-console */
       console.log(`Listening: http://localhost:${port}`);
       /* eslint-enable no-console */
+
     });
   } catch (error) {
     console.log('Server error', (error as Error).message);
